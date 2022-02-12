e7e77952b7bf2ef99461732c6121c6e25edffcff
Lihini98 <85431798+Lihini98@users.noreply.github.com>
2022-02-12 16:13:46 +0530

    create Content.jsx

---
+++ src/UIElements/Content/Content.jsx
@@ -0,0 +1,26 @@
+import './Content.css';
+
+const Content = () => {
+    return(
+        <div className='content-wrapper'>
+                     
+            <label> Crazy Corders: </label>
+            <input type="text" name="crazy-corders" /><br/><br/>
+
+            <label> Bit Devs: </label>
+            <input type="text" name="bit-devs" /><br/><br/>
+
+            <label> Crew 4: </label>
+            <input type="text" name="crew-4" /><br/><br/>
+
+            <label> Team Clough: </label>
+            <input type="text" name="team-clough<" /><br/><br/>
+
+            <label> Evil Geniuses: </label>
+            <input type="text" name="evil-geniuses" /><br/>
+           
+        </div>
+    )
+}
+
+export default Content;
