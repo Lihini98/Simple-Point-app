2830d2038534568e617a54a735419ad3996ac974
Lihini98 <85431798+Lihini98@users.noreply.github.com>
2022-02-12 16:15:16 +0530

    Create Heading.jsx

---
+++ src/UIElements/Heading/Heading.jsx
@@ -0,0 +1,13 @@
+import './Heading.css';
+
+const Heading = () => {
+
+    return(
+        <div className='heading-topic'>
+            <h2 className='heading-h2'>Team Scores</h2>
+        </div>
+    )
+
+}
+
+export default Heading;
